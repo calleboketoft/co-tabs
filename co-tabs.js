53628c85af7e12c43cf698f1765d495f325ee05a
Carl-Ola Boketoft <calleboketoft@gmail.com>
2016-04-22 12:12:01 +0200

    compile

--- co-tabs.js
+++ co-tabs.js
@@ -3,4 +3,4 @@ var co_tabs_cmp_1 = require('./src/co-tabs/co-tabs-cmp');
 exports.CoTabsCmp = co_tabs_cmp_1.CoTabsCmp;
 var co_tab_cmp_1 = require('./src/co-tabs/co-tab-cmp');
 exports.CoTabCmp = co_tab_cmp_1.CoTabCmp;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY28tdGFicy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImNvLXRhYnMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLDRCQUF3QiwyQkFDeEIsQ0FBQztBQURPLDRDQUEyQztBQUNuRCwyQkFBdUIsMEJBQTBCLENBQUM7QUFBMUMseUNBQXlDIn0=
+//# sourceMappingURL=co-tabs.js.map
